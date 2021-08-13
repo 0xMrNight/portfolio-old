8eb00bfb1f24e6204b026efd41d55b7aac6157d4
Siddharth Arumugam <0xmrnight@gmail.com>
2021-08-13 11:55:24 +0530

    Update 404

--- pages/404.js
+++ pages/404.js
@@ -1,14 +1,24 @@
 import styles from "../styles/404.module.css";
+import Head from "next/head";
 import Link from "next/link";
 
 export default function custom404() {
   return (
-    <div className={styles.container}>
-      <h1>404 - Page not found</h1>
-      <h3 className={styles.desc}>
-        {" "}
-        Return to the <Link href="/"> Home page </Link>{" "}
-      </h3>
-    </div>
+    <>
+      <Head>
+        <meta name="robots" content="noindex, nofollow" />
+        <title> 404 - Page Not Found </title>
+      </Head>
+      <div className={styles.container}>
+        <h1>
+          <span className="text-red">404</span> -{" "}
+          <span className="text-green">Page not found</span>
+        </h1>
+        <h3 className={styles.desc}>
+          {" "}
+          Return to the <Link href="/"> Home page </Link>{" "}
+        </h3>
+      </div>
+    </>
   );
 }
