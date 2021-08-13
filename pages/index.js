fa730ebc16ff63e4f5775a4c8e126ed891fe2688
Siddharth Arumugam <0xmrnight@gmail.com>
2021-08-13 11:56:40 +0530

    Use custom Head component for index

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,14 @@
-import Head from "next/head";
+import Head from "../components/Head";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
-      <Head>
-        <meta
-          name="description"
-          content="I am Siddharth Arumugam - Full stack Web Dev and Cyber Sec researcher."
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-
-        <meta name="author" content="Siddharth Arumugam" />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@0xMrNight" />
-        <meta name="twitter:creator" content="@0xMrNight" />
-        <meta property="og:url" content="https://mrnight.tk/" />
-        <title> Siddharth Arumugam </title>
-      </Head>
+      <Head
+        title="Siddharth Arumugam"
+        desc="Siddharth Arumugam - Full stack Web Dev | Cyber Sec researcher."
+        og_url="https://mrnight.tk/"
+      />
       <main>
         <div className={styles.title}>
           {" "}
