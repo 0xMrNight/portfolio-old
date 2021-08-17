57531b90cd3402ea88b5dbedbbe67a470f209d46
Siddharth Arumugam <0xmrnight@gmail.com>
2021-08-17 11:17:35 +0530

    Update home
    * Add logo image
    
    * Add description

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from "../components/Head";
+import Img from "next/image";
+
+import Logo from "../public/img/logo.jpg";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head
         title="Siddharth Arumugam"
         desc="Siddharth Arumugam - Full stack Web Dev | Cyber Sec researcher."
@@ -11,12 +14,28 @@ export default function Home() {
         og_url="https://mrnight.tk/"
         og_image="/img/logo.jpg"
       />
-      <main>
-        <div className={styles.title}>
-          {" "}
-          Hi, I&apos;m <span className={styles.name}> Sid </span>{" "}
+      <main className={styles.main}>
+        <div className={styles.intro}>
+          <div className={styles.logo}>
+            <Img src={Logo} alt="My Logo" />
+          </div>
+          <div className={styles.title}>
+            {" "}
+            Hi, I&apos;m <span className={styles.name}> Sid </span>{" "}
+          </div>
+          <div className={styles.desc}>
+            <p>
+              {" "}
+              Siddharth Arumugam (
+              <a href="https://twitter.com/0xMrNight" rel="noopener">
+                <code>0xMrNight</code>
+              </a>
+              ) is a Full stack web developer, Cybersecurity researcher and
+              Linux enthusiast.{" "}
+            </p>
+          </div>
         </div>
       </main>
-    </div>
+    </>
   );
 }
