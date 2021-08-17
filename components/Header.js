bbea233ff576fb0645169d577bb990aefdfda117
Siddharth Arumugam <0xmrnight@gmail.com>
2021-08-17 13:38:19 +0530

    Add header component

---
+++ components/Header.js
@@ -0,0 +1,34 @@
+import styles from "../styles/Header.module.css";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.inner}>
+        <div className={styles.logo}>
+          <Link href="/"> 0xMrNight </Link>
+        </div>
+      </div>
+
+      <div className={styles.menu}>
+        <ul className={styles.menu_inner}>
+          {/* Todo: Make about and contact page */}
+          <li>
+            {" "}
+            <Link href="https://blog.mrnight.tk/about"> About </Link>{" "}
+          </li>
+          <li>
+            {" "}
+            <Link href="https://blog.mrnight.tk/contact"> Contact </Link>{" "}
+          </li>
+          <li>
+            {" "}
+            <Link href="https://blog.mrnight.tk/"> Blog </Link>{" "}
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
